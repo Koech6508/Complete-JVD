b1efda0ed67993bc13bca4b363a6d4c8428283de
Koech6508 <koechdebora@gmail.com>
2024-02-14 15:02:05 +0300

    interest calculation

---
+++ Interest.java
@@ -0,0 +1,15 @@
+
+public class Interest {
+    public static void main(String[] args) {
+        double amount;
+        double rate = 0.05;
+        double principal = 1000.0;
+
+        System.out.printf("%s%20s%n","year","amount on deposit");
+
+        for (int year = 1; year <=10; year++){
+            amount = principal * Math.pow(1.0+rate,year);
+            System.out.printf("%4d% ,20.2f%n",year,amount);
+        }
+    }
+}
